9362a571ef5edf029aa94d0f5ac305e40d1c86d1
kiitan087 <kiitan087@gmail.com>
2021-06-28 17:37:06 +0900

    rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,24 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   with_options presence: true do
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/, message: "is invalid. Input half-width alphanumeric mixed characters."}
+    validates :password,
+              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/,
+                        message: 'is invalid. Input half-width alphanumeric mixed characters.' }
     validates :last_name
     validates :first_name
     validates :last_name_kana
     validates :first_name_kana
     validates :nickname
     validates :birthday
-    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "is invalid. Input '@' character."}
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "is invalid. Input '@' character." }
   end
 
-  with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "is invalid. Input full-width characters."} do
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' } do
     validates :last_name
     validates :first_name
   end
 
-  with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} do
+  with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' } do
     validates :last_name_kana
     validates :first_name_kana
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
-    password_confirmation {password}
-    last_name             {"花子"}
-    first_name            {"佐藤"}
-    last_name_kana        {"ハナコ"}
-    first_name_kana       {"サトウ"}
-    birthday              {Faker::Date.between(from: '1930-01-01', to: '2016-12-31')}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
+    password_confirmation { password }
+    last_name             { '花子' }
+    first_name            { '佐藤' }
+    last_name_kana        { 'ハナコ' }
+    first_name_kana       { 'サトウ' }
+    birthday              { Faker::Date.between(from: '1930-01-01', to: '2016-12-31') }
   end
 end
