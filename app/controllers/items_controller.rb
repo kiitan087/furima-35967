62e753d9241b8892aaa92700a1b17fdf9ef99d6d
kiitan087 <kiitan087@gmail.com>
2021-06-28 18:55:40 +0900

    Active Strageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,8 @@ class ItemsController < ApplicationController
   end
   def create
   end
+
+  #def message_params
+    #params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  #end
 end
