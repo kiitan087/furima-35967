2b00450040284ab6c4d73c6043e76dd24f9f2d3e
kiitan087 <kiitan087@gmail.com>
2021-06-29 21:05:43 +0900

    テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,15 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
+    validates :image
+    validates :product
+    validates :product_description
     validates :category_id
     validates :status_id
     validates :delivery_change_id
     validates :shipping_area_id
     validates :delivery_time_id
-    validates :price
+    validates :price, numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
   end
 
   with_options numericality: { other_than: 1 } do
@@ -25,4 +28,7 @@ class Item < ApplicationRecord
     validates :shipping_area_id
     validates :delivery_time_id
   end
+
+  validates :price, numericality: {with: /\A[0-9]+\z/, message: 'is invalid. Input half-width numeric characters.'}
+  
 end
