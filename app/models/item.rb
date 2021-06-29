cc9e8da157d8226b705bee8d580e67eec0656ded
kiitan087 <kiitan087@gmail.com>
2021-06-29 12:49:05 +0900

    active hash導入、id設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,24 @@
-class Item < ApplicationRecord
+class Article < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  belongs_to :category
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :category
+    validates :status
+    validates :delivery_change
+    validates :shipping_area
+    validates :delivery_time
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category
+    validates :status
+    validates :delivery_change
+    validates :shipping_area
+    validates :delivery_time
+  end
 end
+
