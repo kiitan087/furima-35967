b02a2159c8c41ae13c7f77f76fa4d9b76edae1cd
kiitan087 <kiitan087@gmail.com>
2021-07-06 17:39:21 +0900

    formオブジェクトパターンの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
   end
 
   def new
+    @order_destination = OrderDestination.new
   end
 
   def create
+    @order_destination = OrderDestination.new(order_params)
+    if @order_destination.valid?
+      @order_destination.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+  private
+
+  def order_params
+    params.require(:order_destination).permit(:zip, :shipping_area_id, :city, :block, :building, :phone_number, :user_id, :item_id).merge(user_id: current_user.id)
+  end
 end
--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -10,4 +10,9 @@ class OrderDestination
   validates :shipping_area_id, numericality {other_than: 1}
   validates :phone_number, numericality format: {with: /\A\d{10,11}\z/, message: "is invalid. Input half-width numeric characters(10 or 11 digits)."}
   end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Destination.create(zip: zip, shipping_area_id: shipping_area_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order_id)
+  end
 end
